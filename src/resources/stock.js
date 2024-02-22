723040ae78ec4d213670295c8f92ee11bfea90d7
Sarah Squyres <ssquyres15@gmail.com>
2024-02-22 15:55:50 -0600

    adding notes

--- src/resources/stock.js
+++ src/resources/stock.js
@@ -1,12 +1,16 @@
 // Fetches stock data from IEX Cloud API
+
+// imports config details 
 import { iex } from "../config/iex.js";
 
 export const stock = {
-    // Query API, ticker and callback will be passed in
+    // Fetches the latest price data for a given stock, ticker and callback will be passed in
     latestPrice: (ticker, callback) => {
-        // Will return promise, now able to chain another event
+        // Constructs API URL using latestpriceURL and makes a fetch request to URL
         fetch(stock.latestpriceURL(ticker))
+            // Parses response as JSON
             .then((response) => response.json())
+            // latestPrice function is done, now callback function (formatPriceData) will run
             .then((data) => {
                 callback(stock.formatPriceData(data))
             })
@@ -16,20 +20,26 @@ export const stock = {
         return `${iex.base_url}/data/CORE/INTRADAY_PRICES/${ticker}?&token=${iex.api_token}`
     },
 
-    // Structures data to access relevant data
+    // Extract relevant information from the raw API response
     formatPriceData: (data) => {
-        const stockData = data[data.length - 1]
+        // Extracts the second-to-last element from the data array and assigns it to the stockData
+        const stockData = data[data.length - 2]
         const formattedData = {}
         formattedData.price = stockData.close
         formattedData.date = stockData.date
         formattedData.time = stockData.label
+        // Returns a formatted object
         return formattedData
     },
 
     // Get day open data to calculate day percent and price change 
     getTodaysOpen: (ticker, callback) => {
+        // Request data from API
         fetch(stock.todaysOpenURL(ticker))
+            // Converts response to JSON
             .then((response) => response.json())
+            // What to do when data comes back
+            // getTodaysOpen function is done, now callback function (formatPriceDataOpen) will run
             .then((data) => {
                 callback(stock.formatPriceDataOpen(data))
             })
@@ -40,11 +50,13 @@ export const stock = {
     },
 
     formatPriceDataOpen: (data) => {
+        // Extracts the first element from the data array and assigns it to the firstStockData
         const firstStockData = data[0]
         const firstFormattedData = {}
         firstFormattedData.price = firstStockData.close
         firstFormattedData.date = firstStockData.date
         firstFormattedData.time = firstStockData.label
+        // Returns a formatted object
         return firstFormattedData
     }
 }
