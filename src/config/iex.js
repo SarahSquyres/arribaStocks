35daa417f127e174455952452030944982715259
Sarah Squyres <ssquyres15@gmail.com>
2024-03-25 23:04:25 -0500

    updated token

--- src/config/iex.js
+++ src/config/iex.js
@@ -1,5 +1,5 @@
 // Public API key
 export const iex = {
-    api_token: "pk_c06c841d91b84bdbbfe8077fc67ded57",
+    api_token: "pk_b08a062525754c6bae552701a3d34872",
     base_url: "https://api.iex.cloud/v1/"
 };
